763dd7567af0e10e0b20ffa1753c910b5247f110
adryanlops <adylopesk8@gmail.com>
2025-10-29 13:28:25 -0300

    alteração da atividade 02

--- atividade02.js
+++ atividade02.js
@@ -1,14 +1,17 @@
 const leia = require('readline-sync')
 
-let nota1, nota2, nota3, nota4, media
-
 console.log("\nVamos calcular sua média!\n")
 
-nota1 = leia.questionFloat("Digite a primeira nota: ")
-nota2 = leia.questionFloat("Digite a segunda nota: ")
-nota3 = leia.questionFloat("Digite a tereceira nota: ")
-nota4 = leia.questionFloat("Digite a quarta nota: ")
+let notas = []
+let soma = 0
 
-media = (nota1 + nota2 + nota3 + nota4)/4
+for(let i = 0; i<4; i++){
+ notas[i] = leia.questionFloat("Digite sua nota aqui: ")
+soma += notas[i]
+}
 
-console.log("\nSua média é de :", media)
+let media = soma / notas.length
+media = media.toFixed(1)
+
+console.log("Sua média total é de: ",media)
+
